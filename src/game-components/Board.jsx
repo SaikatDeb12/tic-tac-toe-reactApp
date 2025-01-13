bc2ee6507f5dbb76888c332d4ce3825eccf8f191
SaikatDeb12 <saikat.rcld@gmail.com>
2025-01-13 14:39:04 +0530

    added board, basic UI done

--- src/game-components/Board.jsx
+++ src/game-components/Board.jsx
@@ -2,7 +2,40 @@ import React, { useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
-  return <div></div>;
+  const [state, setState] = useState(Array(9).fill(null));
+  const [turn, changeTurn] = useState(true);
+
+  const squareClicked = (ind) => {
+    // console.log(ind);
+    const tempState = [...state];
+    if (turn) {
+      tempState[ind] = "X";
+    } else {
+      tempState[ind] = "O";
+    }
+    changeTurn(!turn);
+    setState(tempState);
+  };
+
+  return (
+    <div className="board-container">
+      <div className="board-rows">
+        <Square onClick={() => squareClicked(0)} value={state[0]} />
+        <Square onClick={() => squareClicked(1)} value={state[1]} />
+        <Square onClick={() => squareClicked(2)} value={state[2]} />
+      </div>
+      <div className="board-rows">
+        <Square onClick={() => squareClicked(3)} value={state[3]} />
+        <Square onClick={() => squareClicked(4)} value={state[4]} />
+        <Square onClick={() => squareClicked(5)} value={state[5]} />
+      </div>
+      <div className="board-rows">
+        <Square onClick={() => squareClicked(6)} value={state[6]} />
+        <Square onClick={() => squareClicked(7)} value={state[7]} />
+        <Square onClick={() => squareClicked(8)} value={state[8]} />
+      </div>
+    </div>
+  );
 };
 
 export default Board;
